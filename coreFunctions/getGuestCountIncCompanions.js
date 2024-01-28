030d725de12397137428e25190282279aa687fdd
VishnuNKrishnan <navaneeth.vishnu1@gmail.com>
2024-01-28 22:03:23 +0400

    count incl companion logic change

--- coreFunctions/getGuestCountIncCompanions.js
+++ coreFunctions/getGuestCountIncCompanions.js
@@ -20,9 +20,11 @@ const getGuestCountIncCompanions = ({
                 var totalCount = 0
 
                 result.forEach(obj => {
+                    if (obj.attendance == 'will') {
+                        totalCount += 1
+                    }
 
                     if (obj.companions !== '' && obj.attendance == 'will') {
-                        totalCount += 1
                         totalCount += obj.companions.split(',').length
                     }
                 })
